37262d7e3d8195f5d379b0fffe5b72922f91079a
OGBONNA SUNDAY <62995161+OgDev-01@users.noreply.github.com>
2023-02-08 23:16:58 +0100

    feat: setup shadcn ui library (#11)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,28 @@
-import './globals.css'
+import { Inter, Chivo_Mono } from "@next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+    variable: "--font-inter",
+    display: "optional"
+});
+const chivo_mono = Chivo_Mono({
+    variable: "--font-chivo-mono",
+    display: "optional"
+});
 
 export default function RootLayout({
-  children,
+    children
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en" className={`${inter.variable} ${chivo_mono.variable}`}>
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body>{children}</body>
+        </html>
+    );
 }
