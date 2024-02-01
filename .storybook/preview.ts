1aa5aafa8b344a3dc5ae6d754cb588e99b6e553e
Sunday Ogbonna <oliviamegan11@gmail.com>
2024-02-01 17:18:47 +0100

    feat: add storybook testing to project

---
+++ .storybook/preview.ts
@@ -0,0 +1,27 @@
+import type { Preview, ReactRenderer } from "@storybook/react";
+import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByDataAttribute<ReactRenderer>({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+      attributeName: "data-theme",
+    }),
+  ],
+};
+
+export default preview;
