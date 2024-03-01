557b081ba74f05a75ee4c14c179b6bc16757c1cc
Sunday Ogbonna <oliviamegan11@gmail.com>
2024-03-01 20:37:29 +0100

    fix: build error

--- src/backend/config/db.ts
+++ src/backend/config/db.ts
@@ -2,8 +2,14 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const dbConnectionString = process.env.NEON_DB_CONNECTION_STRING as string;
+// Workaround until Neon Vercel integration adds this query parameter
+if (!process.env.NEON_DB_CONNECTION_STRING) {
+  throw new Error("process.env.DATABASE_URL was missing");
+}
+const url = new URL(process.env.NEON_DB_CONNECTION_STRING);
+url.searchParams.set("sslmode", "require");
 
+const dbConnectionString = url.toString();
 const sql = neon(dbConnectionString);
 
 export const db = drizzle(sql, { schema });
