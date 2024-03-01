dc486061b3c561b342040981173a68872226435a
Sunday Ogbonna <oliviamegan11@gmail.com>
2024-03-01 20:46:02 +0100

    fix: trying to fix database error

--- src/backend/config/db.ts
+++ src/backend/config/db.ts
@@ -1,12 +1,14 @@
-import { neon } from "@neondatabase/serverless";
+import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
+neonConfig.fetchConnectionCache = true; // Enable the connection cache
+
 // Workaround until Neon Vercel integration adds this query parameter
-if (!process.env.NEON_DB_CONNECTION_STRING) {
+if (!process.env.DATABASE_URL) {
   throw new Error("process.env.DATABASE_URL was missing");
 }
-const url = new URL(process.env.NEON_DB_CONNECTION_STRING);
+const url = new URL(process.env.DATABASE_URL);
 url.searchParams.set("sslmode", "require");
 
 const dbConnectionString = url.toString();
