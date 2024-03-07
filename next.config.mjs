af6e818942657c6c65d09ab50c3d0e6f9c9f2b4b
Sunday Ogbonna <oliviamegan11@gmail.com>
2024-03-07 15:35:13 +0100

    chore: remove redirects from next-config

--- next.config.mjs
+++ next.config.mjs
@@ -11,31 +11,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
